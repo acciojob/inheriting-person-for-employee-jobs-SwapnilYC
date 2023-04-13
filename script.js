960c66727debea76f1f9597f6b548f469273bb70
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-04-13 16:59:56 +0530

    Constructor functions updated

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
 
-function Employee(name, age, jobTitle) {}
+}
 
+Person.prototype.greet = () => {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+}
+
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+Employee.prototype.jobGreet = () => {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+}
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
